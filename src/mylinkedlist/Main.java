e3e9230e4480c8259824f72552d53d3d387ffe7c
KhanhDang <emailcuadang1705@gmail.com>
2024-11-28 17:12:22 +0700

    "Khanh Dang da update version 1"

--- src/mylinkedlist/Main.java
+++ src/mylinkedlist/Main.java
@@ -1,4 +1,54 @@
 package mylinkedlist;
 
 public class Main {
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.addFirst(1);
+        linkedList.addFirst(2);
+        linkedList.addFirst(3);
+
+        linkedList.addLast(4);
+        linkedList.addLast(5);
+        linkedList.addLast(6);
+
+        System.out.println("Do dai la: " + linkedList.length());
+
+        System.out.print("Danh sach la: ");
+        linkedList.display();
+
+        linkedList.add(0, 3);
+
+        System.out.print("Danh sach sau khi them la: ");
+        linkedList.display();
+
+        linkedList.deleteFirst();
+        System.out.print("Danh sach da xoa phan tu dau: ");
+        linkedList.display();
+
+        linkedList.deleteLast();
+        System.out.print("Danh sach da xoa phan tu cuoi: ");
+        linkedList.display();
+
+        linkedList.delete(2);
+        System.out.print("Danh sach da xoa phan tu mong muon: ");
+        linkedList.display();
+
+        boolean found = linkedList.search(0);
+        if (found){
+            System.out.println("Co du lieu.");
+        } else {
+            System.out.println("Khong co du lieu.");
+        }
+
+        Node nodeAtPosition = linkedList.searchAtPosition(1);
+        if (nodeAtPosition != null){
+            System.out.println("Phan tu da tim thay la: " + nodeAtPosition.getData());
+        } else {
+            System.out.println("Khong tim thay du lieu.");
+        }
+
+        linkedList.sort();
+        System.out.print("Danh sach da sap xep: ");
+        linkedList.display();
+    }
 }
--- src/mylinkedlist/MyLinkedList.java
+++ src/mylinkedlist/MyLinkedList.java
@@ -1,4 +1,151 @@
 package mylinkedlist;
 
+import javax.management.StandardMBean;
+
 public class MyLinkedList {
+    private Node head;
+
+    public void display(){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.getData());
+            if (temp.getNext() != null){
+                System.out.print("->");
+            }
+            temp = temp.getNext();
+        }
+        System.out.println();
+    }
+
+    public int length(){
+        Node temp = head;
+        int count = 0;
+        while (temp != null){
+            count++;
+            temp = temp.getNext();
+        }
+        return count;
+    }
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        newNode.setNext(head);
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if (head == null){
+            head = newNode;
+            return;
+        } else {
+            Node temp = head;
+            while (temp.getNext() != null){
+                temp = temp.getNext();
+            }
+            temp.setNext(newNode);
+        }
+    }
+
+    public void add(int data, int index){
+        Node newNode = new Node(data);
+        if (index == 0){
+            addFirst(data);
+            return;
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index - 1 ; i++){
+                temp = temp.getNext();
+            }
+            Node nextNode = temp.getNext();
+            temp.setNext(newNode);
+            newNode.setNext(nextNode);
+        }
+    }
+
+    public void deleteFirst(){
+        if (head != null){
+            head = head.getNext();
+        }
+    }
+
+    public void deleteLast(){
+        if (head == null){
+            return;
+        } else if (head.getNext() == null){
+            deleteFirst();
+        } else {
+            Node temp = head;
+            Node preTemp = null;
+            while(temp.getNext() != null){
+                preTemp = temp;
+                temp = temp.getNext();
+            }
+            preTemp.setNext(null);
+        }
+    }
+
+    public void delete(int index){
+        if (index == 0){
+            deleteFirst();
+        } else if (index == length() - 1){
+            deleteLast();
+        } else {
+            int count = 0;
+            Node temp = head;
+            while (count < index - 1){
+                count++;
+                temp = temp.getNext();
+            }
+            Node nextNode = temp.getNext();
+            temp.setNext(nextNode.getNext());
+        }
+    }
+
+    public boolean search(int data){
+        if (head == null){
+            return false;
+        } else {
+            Node temp = head;
+            while (temp != null){
+                if (temp.getData() == data){
+                    return true;
+                }
+                temp = temp.getNext();
+            }
+        }
+        return false;
+    }
+
+    public Node searchAtPosition(int index){
+        if (index < 0 || index >= length()){
+            return null;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++){
+            temp = temp.getNext();
+        }
+        return temp;
+    }
+
+    public void sort(){
+        if (head == null || head.getNext() == null){
+            return;
+        } else {
+            boolean swapped;
+            do {
+                swapped = false;
+                Node temp = head;
+                while (temp != null && temp.getNext() != null){
+                    if (temp.getData() > temp.getNext().getData()){
+                        int tempData = temp.getData();
+                        temp.setData(temp.getNext().getData());
+                        temp.getNext().setData(tempData);
+                        swapped = true;
+                    }
+                    temp = temp.getNext();
+                }
+            } while (swapped);
+        }
+    }
 }
